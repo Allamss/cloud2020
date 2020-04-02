ad608599569ecb75ea9450aab92b60d8c75081b4
Allamss <522458390@qq.com>
2020-04-02 14:47:11 +0800

    nacos配置中心测试

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/allams/springcloud/NocosConfigClientMain3377.java
@@ -0,0 +1,16 @@
+package com.allams.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Allams
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NocosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NocosConfigClientMain3377.class, args);
+    }
+}
