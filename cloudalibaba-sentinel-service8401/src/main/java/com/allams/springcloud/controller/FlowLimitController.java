5b72505c8e4525307906abd000271242e1c4b230
Allamss <522458390@qq.com>
2020-04-08 20:56:35 +0800

    sentinel热点控制上手测试

--- cloudalibaba-sentinel-service8401/src/main/java/com/allams/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/allams/springcloud/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.allams.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,6 +20,17 @@ public class FlowLimitController {
     @GetMapping(value = "/testB")
     public String testB() {
         return "---testB";
+    }
+
+    @GetMapping(value = "/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealTestHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false)String p1,
+                             @RequestParam(value = "p2", required = false)String p2) {
+        return "testHotKey";
+    }
+
+    public String dealTestHotKey(String p1, String p2, BlockException exception) {
+        return "俺来兜底了";
     }
 
 }
